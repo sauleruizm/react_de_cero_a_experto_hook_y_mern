f397f06982a8a554abe1abc5ef9d6eeba0b98fb7
Saúl Ruíz <sauleruizm@gmail.com>
2021-10-13 21:46:28 -0500

    ejemplo 5 del curso

---
+++ 05-hook-app/src/components/01-useState/CounterApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const CounterApp = () => {
+    return (
+        <>
+          <h1>Counter { 0 }</h1>  
+          <hr/>
+          <button className="btn btn-primary">+1</button>
+
+        </>
+    )
+}
