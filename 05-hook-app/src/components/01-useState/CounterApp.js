6b3a63453cfc4e504abca220de9c9a1823d831e7
Saúl Ruíz <sauleruizm@gmail.com>
2021-10-26 18:28:35 -0500

    guardando avance

--- 05-hook-app/src/components/01-useState/CounterApp.js
+++ 05-hook-app/src/components/01-useState/CounterApp.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './counter.css';
 
 export const CounterApp = () => {
+
+  const [state, setCounter] = useState({
+    counter1: 10,
+    counter2:20
+  });
+
+  const { counter1, counter2 } = state;
     return (
         <>
-          <h1>Counter { 0 }</h1>  
+          <h1>Counter1 { counter1 }</h1>  
+          <h1>Counter2 { counter2 }</h1>  
           <hr/>
-          <button className="btn btn-primary">+1</button>
+          <button className="btn btn-primary"
+            onClick={() => setCounter(
+              {
+                ...state,
+                counter1: counter1 + 1
+              }
+            ) }>+1</button>
 
         </>
     )
