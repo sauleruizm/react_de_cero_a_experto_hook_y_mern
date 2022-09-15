8eab0522f5b6557c44abb03e638875f28753e3d0
Saúl Ruíz M <sauleruizm@gmail.com>
2022-09-15 01:21:36 -0500

    feat(sec-10): cap 146

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -1,24 +1,39 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { TodoAdd } from './TodoAdd'
 import { TodoList } from './TodoList'
 import { todoReducer } from './TodoReducer'
 
 export const initialState = [
-  {
-    id: new Date().getTime(),
-    description: ' Recolectar la piedra del alma',
-    done: false,
-  },
-  {
-    id: new Date().getTime() * 3,
-    description: ' Recolectar la piedra del tiempo',
-    done: false,
-  },
+  // {
+  //   id: new Date().getTime(),
+  //   description: ' Recolectar la piedra del alma',
+  //   done: false,
+  // },
+  // {
+  //   id: new Date().getTime() * 3,
+  //   description: ' Recolectar la piedra del tiempo',
+  //   done: false,
+  // },
 ]
+
+
+const init = () => {
+  console.log(localStorage.getItem('todos'))
+  return JSON.parse(localStorage.getItem('todos')) || []
+}
 
 export const TodoApp = () => {
 
-  const [todos, dispatch] = useReducer(todoReducer, initialState)
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init)
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  
+    return () => {
+      
+    }
+  }, [todos])
+  
 
   const handleNewTodo =(todo)=>{
     console.log({todo})
