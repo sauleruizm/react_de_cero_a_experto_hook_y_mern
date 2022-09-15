623eeb9e8921b0ac0e3e6a0b23b24d16d223aead
Saúl Ruíz M <sauleruizm@gmail.com>
2022-09-15 02:06:10 -0500

    feat(sec-10): cap-148

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -33,10 +33,19 @@ export const TodoApp = () => {
       
     }
   }, [todos])
+
+
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type:'[TODO] Remove Todo',
+      payload: id
+    })
+    // console.log(id)
+  }
   
 
   const handleNewTodo =(todo)=>{
-    console.log({todo})
+    // console.log({todo})
     const action = {
       type:'[TODO] Add Todo',
       payload: todo
@@ -45,13 +54,24 @@ export const TodoApp = () => {
     dispatch(action)
   }
 
+  const handleToggleTodo = (id) => {
+    // console.log(id)
+    dispatch({
+      type:'[TODO] Toggle Todo',
+      payload: id
+    })
+  }
+
   return (
     <>
       <h1>TodoApp (10), <small>Pendientes: 2</small></h1>
       <hr/>
       <div className='row'>
         <div className='col-7'>
-          <TodoList todos={todos}/>
+          <TodoList todos={todos} 
+            onDeleteTodo={handleDeleteTodo}
+            onToggleTodo={handleToggleTodo}
+          />
         </div>
         <div className='col-5'>
           <h4>Agregar TODO</h4>
--- 05-hook-app/src/08-useReducer/TodoList.jsx
+++ 05-hook-app/src/08-useReducer/TodoList.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { TodoItem } from './TodoItem'
 
-export const TodoList = ({todos = []}) => {
+export const TodoList = ({todos = [], onDeleteTodo, onToggleTodo}) => {
   return (
     <ul className='list-group'>
     {
       todos.map( todo => (
         //todoitem
-        <TodoItem key={todo.id +1} todo={todo}/>
+        <TodoItem key={todo.id +1} todo={todo} 
+          onDeleteTodo={onDeleteTodo}
+          onToggleTodo={onToggleTodo}
+        />
       ))
     }
   </ul>
