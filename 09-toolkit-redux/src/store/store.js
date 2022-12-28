b2e487df643779b55313371a425c4ab9cecaad7b
Saúl Ruíz M <sauleruizm@gmail.com>
2022-12-28 01:26:36 -0500

    seccion 18: cap 262

---
+++ 09-toolkit-redux/src/store/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { counterSlice}  from './slices/counter/counterSlice'
+export const store = configureStore({
+  reducer: {
+
+    counter: counterSlice.reducer,
+
+  },
+})
