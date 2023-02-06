3a2788a9f939ec2b21b2a73b77f93b8d316bc046
Saúl E. Ruíz M <sruiz@copaair.com>
2023-02-06 16:55:32 -0500

    feat: add sección 19, 274

--- 08-journal-app/src/main.jsx
+++ 08-journal-app/src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import ReactDOM from 'react-dom/client'
-import JournalApp from './JournalApp'
 import './styles.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import JournalApp from './JournalApp'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
