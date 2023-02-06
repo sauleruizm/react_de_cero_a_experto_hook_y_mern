497ab831f958b7b2e5e39d14083dd0557a0ae8c2
Saúl E. Ruíz M <sruiz@copaair.com>
2023-02-06 17:04:07 -0500

    feat: cap. 275

---
+++ 08-journal-app/src/store/auth/authSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  status: 'checking', //not-authinticated, authenticated
+  uid: null,
+  email: null,
+  displayName: null,
+  photoURL: null,
+  errorMessage: null,
+}
+
+export const authSlice = createSlice({
+  name: 'name',
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      
+    },
+    logout: (state, payload) => {
+    
+    },
+    checkingCredentials: (state) => {
+     
+    }
+ },
+})
+
+export const { login, logout, checkingCredentials } = authSlice.actions
