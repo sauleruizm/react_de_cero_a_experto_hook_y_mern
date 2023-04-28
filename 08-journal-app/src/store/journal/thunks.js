7c8360b0e450208c663bb067d84261f6edc42900
Saúl E. Ruíz M <saul.ruiz@logicstudio.net>
2023-04-28 17:03:14 -0500

    sec 20: cap 302

--- 08-journal-app/src/store/journal/thunks.js
+++ 08-journal-app/src/store/journal/thunks.js
@@ -1,6 +1,7 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite"
 import { FirebaseDB } from "../../firebase/config"
-import { addNewEmptyNote, setActiveNote, savingNewNote } from "./jounalSlice"
+import { addNewEmptyNote, setActiveNote, savingNewNote, setNotes } from "./jounalSlice"
+import { loadNotes } from "../../helpers/loadNotes"
 
 export const startNewNote = () => {
 
@@ -31,5 +32,19 @@ export const startNewNote = () => {
     //dispatch ( newNote )
 
     //dispatch ( activarNote)
+  }
+}
+
+export const startLoadingNotes = ( ) => {
+
+  return async (dispatch, getState) => {
+   
+    const { uid } = getState().auth 
+    if (!uid) throw Error('El UID del usuario no existe')
+
+    const notes = await loadNotes( uid )
+
+    dispatch( setNotes( notes ) )
+    // console.log({uid})
   }
 }
